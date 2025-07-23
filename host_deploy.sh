38d005fe90c642318cdf1e80a4053d4c7635d026
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-07-22 20:22:10 -0700

    Update host_deploy.sh
    added user unput to the shell, added ipmitool and changed yaml config files name to ensure first section after netplan apply is executed.

--- host_deploy.sh
+++ host_deploy.sh
@@ -1,46 +1,42 @@
-#! /usr/bin/env bash
- 
-#For use with Ubuntu Server. Assumes Ubuntu Cloud image and default sudo account. 
-#This script is meant to run with sudo.  
-#sudo bash ~/host_deploy.sh 
-
 #!/bin/bash
 
-GATEWAY_ADDRESS= 
-PRIMARY_DNS_ADDRESS=
-SECONDARY_DNS_ADDRESS=
+ENVIRONMENT=${1:-dev}
 
-ipmitool lan set 1 ipsrc static 
-ipmitool lan set 1 ipaddr 
-ipmitool lan set 1 netmask 
-ipmitool lan set 1 defgw ipaddr 
+GATEWAY=
+PRIMARY_DNS=
+SECONDARY_DNS=
+NETMASK=255.255.254.0
+INTERFACE=$(ls /sys/class/net/ | grep -v lo | head -1)
 
-#Gather input from userits a work in progress
-read -p "Type the IP address in CIDR notation, i.e. 192.168.1.1/24: " IP_ADDRESS 
-#read -p "The gateway IP: " GATEWAY_ADDRESS 
-#read -p "The primary DNS IP: " PRIMARY_DNS_ADDRESS 
-#read -p "And finally, the secondary DNS IP: " SECONDARY_DNS_ADDRESS 
+read -p "Enter the servers assigned static local area network IP address: " LAN_IP
+echo "The servers static local area network address has been set to $LAN_IP"
+read -p "Enter the servers assigned static BMC IP address: " BMC_IP
+echo "The servers static BMC address has been set to $BMC_IP"
 
-# Create a new netplan yaml config file 
-sudo touch ~/99-custom.yaml 
+sudo ipmitool lan set 1 ipsrc static
+ipmitool lan set 1 ipaddr $BMC_IP
+ipmitool lan set 1 netmask $NETMASK
+ipmitool lan set 1 defgw ipaddr $GATEWAY
 
-# Apply network config to netplan yaml config file 
-# Making some assumptions here about the adapter name 
-echo "network:" > ~/99-custom.yaml 
-echo "  ethernets:" >> ~/99-custom.yaml 
-echo "    enp0s31f6:" >> ~/99-custom.yaml 
-echo "      dhcp4: false" >> ~/99-custom.yaml
-echo "      addresses:" >> ~/99-custom.yaml
-echo "       - [$IP_ADDRESS]" >> ~/99-custom.yaml
-echo "      routes:" >> ~/99-custom.yaml
-echo "       - to: default" >> ~/99-custom.yaml
-echo "         via: $GATEWAY_ADDRESS" >> ~/99-custom.yaml
-echo "      nameservers:" >> ~/99-custom.yaml
-echo "        addresses: [$PRIMARY_DNS_ADDRESS, $SECONDARY_DNS_ADDRESS, , ]" >> ~/99-custom.yaml
-echo "  version: 2" >> ~/99-custom.yaml
+# Create a new netplan yaml config file
+sudo touch ~/00-aabash.yaml
 
+# Apply network config to netplan yaml config file. "00-aa" ensures the file is selected 1st alphabetically. 
+echo "network:" > ~/00-aabash.yaml
+echo "  ethernets:" >> ~/00-aabash.yaml
+echo "    $INTERFACE:" >> ~/00-aabash.yaml
+echo "      dhcp4: false" >> ~/00-aabash.yaml
+echo "      addresses:" >> ~/00-aabash.yaml
+echo "        - $LAN_IP/23" >> ~/00-aabash.yaml
+echo "      routes:" >> ~/00-aabash.yaml
+echo "        - to: default" >> ~/00-aabash.yaml
+echo "          via: $GATEWAY" >> ~/00-aabash.yaml
+echo "      nameservers:" >> ~/00-aabash.yaml
+echo "        addresses: [$PRIMARY_DNS, $SECONDARY_DNS,]" >> ~/00-aabash.yaml
+echo "  version: 2" >> ~/00-aabash.yaml
+ 
 #Copy the custom config to the netplan folder and apply
-sudo cp ~/99-custom.yaml /etc/netplan/99-custom.yaml
+sudo cp ~/00-aabash.yaml /etc/netplan/00-aabash.yaml
 
-#Apply the new config
+#Apply the new netplan config
 sudo netplan apply
