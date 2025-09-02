da24558b92c12f0dd238b1d3b2d6f3e4bc380e0b
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-09-02 02:15:13 -0700

    Update subnet_selector.sh

--- subnet_selector.sh
+++ subnet_selector.sh
@@ -11,7 +11,7 @@ while true; do
     read -p "Select the number asscociated with your choice and press enter. " choice
     
     case $choice in
-        1) [ -x "$SNET24" ] && "$SNET24" || echo "Error: $SCRIPT1 not found/executable" ;; #Tests if the file path SNET22 exists AND is executable. 
+        1) [ -x "$SNET24" ] && "$SNET24" || echo "Error: $SCRIPT1 not found/executable" ;; #Tests if the file path SNET24 exists AND is executable. 
         2) [ -x "$SNET23" ] && "$SNET23" || echo "Error: $SCRIPT2 not found/executable" ;; #Tests if the file path SNET23 exists AND is executable. 
         3) exit 0 ;;
         *) echo "Invalid choice" ;;
